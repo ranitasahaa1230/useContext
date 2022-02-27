fab7fc15759a1aa58f869ca60da1ee69837171ea
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2022-02-27 22:52:47 +0530

    Delete createContext.js

--- createContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const cartLogger = () => console.log("carting...");
-
-const CartContext = createContext({ items: 4, logger: cartLogger });
-
-export { CartContext };
